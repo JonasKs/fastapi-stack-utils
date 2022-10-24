5d177b6cd7439426db5356393d8cc71f00f0cf12
Tobias <tobias.bryhn-bjolgerud@intility.no>
2022-10-24 16:18:42 +0200

    feat: add support for fastAPI validation error and nsa server error formats

--- fastapi_stack_utils/schemas/http_exceptions.py
+++ fastapi_stack_utils/schemas/http_exceptions.py
@@ -1,16 +1,26 @@
+from typing import List, Optional
+
 from pydantic import BaseModel, Field
 
 
 class DefaultError(BaseModel):
-    description: str = Field(
-        ...,
+    description: Optional[str] = Field(
+        default=None,
         title='Informative description of the error',
         description='Equal or more verbose error message',
     )
-    error: str = Field(..., title='Object not found', description='Object x does not exist under resource y')
+    error: Optional[str] = Field(
+        default=None, title='Object not found', description='Object x does not exist under resource y'
+    )
 
 
-class ServerError(DefaultError):
+class ValidationError(BaseModel):
+    loc: Optional[List[str]] = Field(default=None, title='Location of error', description='param, subnet_id')
+    msg: Optional[str] = Field(default=None, title='Reason for error', description='Found 2 ranges for 192.168.0.0/24')
+    type: Optional[str] = Field(default=None, title='What type of validation error', description='value_error.invalid')
+
+
+class ServerError(DefaultError, ValidationError):
     ...
 
 
