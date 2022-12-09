0f7861e3ddaf713175c4471e994538bf731fc321
Ingvald Lorentzen <ingvald.lorentzen@intility.no>
2022-12-09 14:45:54 +0100

    Add `fsu` cli Add `load-env` `fsu` command for getting values from vault into .env for Docker Update Flake8 precommit to point to GitHub Bump version

---
+++ fastapi_stack_utils/cli/load_docker_env.py
@@ -0,0 +1,39 @@
+import subprocess
+from subprocess import CalledProcessError
+
+
+def load_docker_env() -> None:
+    """
+    Reads secrets from Vault for project and writes docker specific environment variables to `.env`
+    Checks if user has valid token, if not, triggers loging flow for Vault CLI
+    Uses repo name from git as path for Vault
+    """
+    git_remote = subprocess.run(['git', 'remote', '-v'], check=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+    project_name = (
+        git_remote.split('\n')[0].split()[1].split('@')[1].split(':')[1].split('/')[-1].removesuffix('.git').strip()
+    )
+
+    try:
+        subprocess.run(['vault', 'token', 'lookup'], check=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+    except CalledProcessError:
+        subprocess.run(
+            ['vault', 'login', '-method=oidc', '-path=aa'], check=True, stdout=subprocess.PIPE
+        ).stdout.decode('utf-8')
+
+    secrets = (
+        subprocess.run(
+            ['vault', 'read', '-field=data', '-format=yaml', f'kv-nsa/data/{project_name}/dev/'],
+            check=True,
+            stdout=subprocess.PIPE,
+        )
+        .stdout.decode('utf-8')
+        .replace(': ', '=')
+    )
+
+    docker_secrets = '\n'.join(
+        [secret for secret in secrets.split('\n') if secret.split('=')[0] in ['POSTGRES_PASSWORD', 'REDIS_PASSWORD']]
+    )
+
+    with open('.env', 'w') as docker_env:
+        docker_env.write(docker_secrets + '\n')
